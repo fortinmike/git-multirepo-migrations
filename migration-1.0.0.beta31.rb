a0d11316b15395cf2d57fd7fd46ca85c4c85a37b
Michaël Fortin <fortinmike@irradiated.net>
2015-05-01 22:08:11 -0400

    Added more descriptive logging to migration-1.0.0.beta31.

--- migration-1.0.0.beta31.rb
+++ migration-1.0.0.beta31.rb
@@ -2,11 +2,13 @@ require "fileutils"
 
 class Migration
   def self.run(repo_path)
-    puts "Removing unused hooks..."
+    puts "Deleting .git/hooks/prepare-commit-msg"
     FileUtils.rm_f(".git/hooks/prepare-commit-msg")
+    
+    puts "Deleting .git/hooks/post-merge"
     FileUtils.rm_f(".git/hooks/post-merge")
 
-    puts "Initializing extras..."
+    puts "Initializing extras to add .gitattributes file and add custom merge driver in .git/config"
     system('multi init --extras')
   end
 end
