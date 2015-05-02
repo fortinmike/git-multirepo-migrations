48552e97c62454093e89751a54797da7f04e2427
Michaël Fortin <fortinmike@irradiated.net>
2015-05-01 23:49:28 -0400

    Tweaked migration to beta32 not to perform a multi update.

--- migration-1.0.0.beta32.rb
+++ migration-1.0.0.beta32.rb
@@ -4,8 +4,5 @@ class Migration
   def self.run(repo_path)
     puts "Initializing extras to update .gitattributes for .multirepo.meta auto-merging"
     system('multi init --extras')
-    
-    puts "Updating tracking files to add .multirepo.meta"
-    system('multi update')
   end
 end
