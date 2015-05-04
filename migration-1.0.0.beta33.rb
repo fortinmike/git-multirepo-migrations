160b6a15d9bed989abbcf81d2a91cb4a226f4482
Michaël Fortin <fortinmike@irradiated.net>
2015-05-04 17:51:46 -0400

    Tweaked migration to beta33.

--- migration-1.0.0.beta33.rb
+++ migration-1.0.0.beta33.rb
@@ -2,7 +2,7 @@ require "fileutils"
 
 class Migration
   def self.run(repo_path)
-    puts "Initializing extras to add the post-commit hook which stages tracking files (bug #91565510)"
-    system('multi init --extras')
+    puts "Installing the post-commit hook which stages tracking files (bug #91565510)"
+    system('multi install --hooks')
   end
 end
