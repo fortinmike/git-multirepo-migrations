15ee465101c368193a052cd1f5b0c5accc6fb9fa
Michaël Fortin <michael@simbioz.com>
2015-05-11 13:27:59 -0400

    Using the "git-multirepo" gem for colored console logging and to avoid code duplication.

--- core.rb
+++ core.rb
@@ -6,20 +6,3 @@ def perform_migration(migration_class, repo_path)
   
   Dir.chdir(previous_directory)
 end
-
-def ask_yes_no(message)
-  answered = false
-  while !answered
-    print message
-    print " (y/n) "
-    
-    case $stdin.gets.strip.downcase
-    when "y", "yes"
-      answered = true
-      return true
-    when "n", "no"
-      answered = true
-      return false
-    end
-  end
-end
